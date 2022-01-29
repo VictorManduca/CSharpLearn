12824c886991676c18a0be649879ac01b6436a76
Victor <Victor@DESKTOP-LHKRH09>
2022-01-29 16:27:30 -0300

    Adicionar arquivos de projeto.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("First CShartp program");
+            Console.ReadLine();
+
+            /*
+             * Linguagem
+             * CLR = Common Language Runtime - máquina virtual da plataforma C#, tipo JVM
+             * MSIL = Microsoft Intermediate Language - C# da vida / .net
+             * 
+             * arquivos
+             * .vs = infos do Visual Studio
+             * .sln = solução
+             * .cs = aplicação C#
+             * App.config = arquivo criado junto com o projeto
+             * .csproj = basicamente um xml com configurações ligando os arquivos do projeto, define quem compõe o projeto
+             * 
+             * pastas
+             * Properties = informações do projeto, como sua versão
+             * obj = arquivos temporário em tempo de desenvolvimento do Visual Studio
+             * bin = guarda o executável do projeto
+            */
+        }
+    }
+}
